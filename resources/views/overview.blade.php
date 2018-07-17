f18e20566a17e734747a6595fa18372c6b3e8274
Casper Engelmann <me@casperengelmann.com>
2018-07-17 21:10:11 +0200

    Added Bootstrap 4 support

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -1,22 +1,25 @@
 @extends('master')
 @section('content')
   <main class="container">
-    <div class="row doors">
-      <span class="tile-title-grey" data-title="Kontrolpanel"></span>
-
-      <div class="col s6 m3">
-        <a href="/abonnement" class="white waves-effect waves-dark btn">
+    <h2 class="page-header">Kontrolpanel</h2>
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-3">
+        <a href="/abonnement" class="btn btn-primary tile">
           <i class="material-icons">face</i> Mit Abonnement
+        </a>
+      </div>
+      <div class="col-12 col-sm-6 col-md-3">
+        <a href="/profile" class="btn btn-primary tile">
+          <i class="material-icons">person</i> Min profil
         </a>
       </div>
     </div>
     {{--Paying only--}}
-    <div class="row doors">
-      <span class="tile-title-grey" data-title="Admin"></span>
-
-      <div class="col s6 m3">
-        <a href="Menu link" class="white waves-effect waves-dark btn">
-          <i class="material-icons">person</i> Name
+    <h2 class="page-header">Admin</h2>
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-3">
+        <a href="#" class="btn btn-primary tile">
+          <i class="material-icons">people</i> Brugere
         </a>
       </div>
     </div>
