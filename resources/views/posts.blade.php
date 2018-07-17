e2ae3a0683e7491b77a43951e229b498f4725e68
Casper Engelmann <me@casperengelmann.com>
2018-07-17 18:28:25 +0200

    Added posts template

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,22 @@
+@extends('master')
+@section('content')
+  <div class="container">
+    <div class="row">
+
+      <div class="col s4">
+        <div class="card">
+          <div class="card-content">
+            <span class="card-title">Post Title</span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur magnam exercitationem officiis ipsam asperiores impedit accusamus minima quam. Excepturi labore deleniti modi, minima blanditiis libero recusandae magni mollitia veritatis dolores fuga magnam soluta a ex? Voluptatibus ducimus hic expedita eaque fuga molestias magnam impedit est? Voluptatibus harum dolores ullam nisi?
+            </p>
+          </div>
+          <div class="card-action">
+            <a href="#">Contact Scout</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+@endsection
