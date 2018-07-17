6c46f3a11eeb915c84c337d91f726eafbab3cedf
Casper Engelmann <me@casperengelmann.com>
2018-07-17 18:29:05 +0200

    Changed section titles

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -2,7 +2,7 @@
 @section('content')
   <main class="container">
     <div class="row doors">
-      <span class="tile-title-grey" data-title="Messages"></span>
+      <span class="tile-title-grey" data-title="Kontrolpanel"></span>
 
       <div class="col s6 m3">
         <a href="/abonnement" class="white waves-effect waves-dark btn">
@@ -12,7 +12,7 @@
     </div>
     {{--Paying only--}}
     <div class="row doors">
-      <span class="tile-title-grey" data-title="Team"></span>
+      <span class="tile-title-grey" data-title="Admin"></span>
 
       <div class="col s6 m3">
         <a href="Menu link" class="white waves-effect waves-dark btn">
