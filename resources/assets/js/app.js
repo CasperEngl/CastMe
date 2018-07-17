901da0a1507a77522056d5e5a07ef083c5f77646
Casper Engelmann <me@casperengelmann.com>
2018-07-17 21:57:10 +0200

    Added fileupload functionality

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,3 +6,42 @@
  */
 
 require('./bootstrap');
+
+window.readURL = (input) => {
+  if (input.files && input.files[0]) {
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('.image-upload-wrap').hide();
+
+      $('.file-upload-image').attr('src', e.target.result);
+      $('.file-upload-content').show();
+
+      $('.image-title').html(input.files[0].name);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+
+  } else {
+    removeUpload();
+  }
+}
+
+window.removeUpload = (input) => {
+  input.innerHTML = input.innerHTML;
+}
+
+function removeUpload() {
+  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+  $('.file-upload-content').hide();
+  $('.image-upload-wrap').show();
+}
+
+$('.image-upload-wrap').bind('dragover', function () {
+  $('.image-upload-wrap').addClass('image-dropping');
+});
+
+$('.image-upload-wrap').bind('dragleave', function () {
+  $('.image-upload-wrap').removeClass('image-dropping');
+});
