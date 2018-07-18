98e38a14762f59d705aa15fef942fddb350adf9a
Casper Engelmann <me@casperengelmann.com>
2018-07-18 21:22:57 +0200

    Removed unused modules

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,11 +5,11 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+// Add jQuery to global scope
 global.$ = global.jQuery = require('jquery');
 
 require('popper.js/dist/popper.min');
 require('bootstrap/dist/js/bootstrap.min');
-
 require('tinymce/tinymce.min');
 
 require('./file-input/fileinput-custom');
