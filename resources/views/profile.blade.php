f249eaf255d4c6d2964657e7b390e896a3ae7e74
Casper Engelmann <me@casperengelmann.com>
2018-07-18 22:55:08 +0200

    Added age, height, experience and hair color

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -10,7 +10,7 @@
       </div>
       <div class="col-lg-8">
         <div class="card">
-          <form method="POST">
+          <form method="GET">
             <div class="card-body">
 
               <?php
@@ -49,6 +49,63 @@
                   <div class="form-group">
                     <label for="email" class="text-muted">{{ title_case('Email') }}</label>
                     <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email" value="{{ Auth::User()->email }}">
+                  </div>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col">
+                  <div class="form-group">
+                    <label for="age" class="text-muted">{{ title_case('Age') }}</label>
+                    <input type="number" name="age" class="form-control" id="age" placeholder="27">
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="form-group">
+                    <label for="height" class="text-muted">{{ title_case('Height') }} <small>(in cm)</small></label>
+                    <input type="number" name="height" class="form-control" id="height" placeholder="175 cm">
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="form-group">
+                    <label for="experience" class="text-muted">{{ title_case('Experience') }} <small>(in years)</small></label>
+                    <input type="number" name="experience" class="form-control" id="experience" placeholder="5">
+                  </div>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col">
+                  <div class="form-group">
+                    <label>{{ title_case('Hair color') }}</label>
+                    <ul class="pagination">
+                      <div class="display-none">
+                        <input type="radio" name="hair_color" value="1" id="hair-black"> Black
+                        <input type="radio" name="hair_color" value="2" id="hair-brown" checked> Brown
+                        <input type="radio" name="hair_color" value="3" id="hair-blond"> Blond
+                        <input type="radio" name="hair_color" value="4" id="hair-auburn"> Auburn
+                        <input type="radio" name="hair_color" value="5" id="hair-red"> Red
+                        <input type="radio" name="hair_color" value="6" id="hair-white"> Gray or white
+                      </div>
+                      <li class="page-item">
+                        <label for="hair-black" class="page-link" href="#">Black</label>
+                      </li>
+                      <li class="page-item active">
+                        <label for="hair-brown" class="page-link" href="#">Brown</label>
+                      </li>
+                      <li class="page-item">
+                        <label for="hair-blond" class="page-link" href="#">Blond</label>
+                      </li>
+                      <li class="page-item">
+                        <label for="hair-auburn" class="page-link" href="#">Auburn</label>
+                      </li>
+                      <li class="page-item">
+                        <label for="hair-red" class="page-link" href="#">Red</label>
+                      </li>
+                      <li class="page-item">
+                        <label for="hair-white" class="page-link" href="#">Gray or white<label>
+                      </li>
+                    </ul>
                   </div>
                 </div>
               </div>
