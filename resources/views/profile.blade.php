f4b7104b8bb49960a23625d804d86c4e197b5f68
Casper Engelmann <me@casperengelmann.com>
2018-07-18 20:27:25 +0200

    Removed card-heading

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,55 +3,59 @@
   <main class="container">
     <h2 class="page-header">Profiloplysninger</h2>
 
-    <input id="input-b1" name="input-b1" type="file" class="file">
+    <div class="row">
 
-    <div class="card-heading">
-      <i class="material-icons">arrow_back</i> Profile Settings
-    </div>
-    <div class="card profile-card">
-      <form method="POST">
-        <div class="card-body">
-
-          <div class="row">
-            <div class="col">
-              <div class="form-group">
-                <label for="username" class="text-muted">Username</label>
-                <input type="text" class="form-control" id="username" placeholder="Name yourself">
+      <div class="col-lg-4">
+        <input name="profile-picture-upload" type="file" class="file">
+      </div>
+      <div class="col-lg-8">
+        <div class="card">
+          <form method="POST">
+            <div class="card-body">
+    
+              <div class="row">
+                <div class="col">
+                  <div class="form-group">
+                    <label for="username" class="text-muted">Username</label>
+                    <input type="text" class="form-control" id="username" placeholder="Name yourself">
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-6">
-              <div class="form-group">
-                <label for="username" class="text-muted">First name</label>
-                <input type="text" class="form-control" id="username" placeholder="John/Jane">
+    
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="username" class="text-muted">First name</label>
+                    <input type="text" class="form-control" id="username" placeholder="John/Jane">
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="username" class="text-muted">Last name</label>
+                    <input type="text" class="form-control" id="username" placeholder="Doe">
+                  </div>
+                </div>
               </div>
-            </div>
-            <div class="col-md-6">
-              <div class="form-group">
-                <label for="username" class="text-muted">Last name</label>
-                <input type="text" class="form-control" id="username" placeholder="Doe">
+    
+              <div class="row">
+                <div class="col">
+                  <div class="form-group">
+                    <label for="email" class="text-muted">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="Enter your email">
+                  </div>
+                </div>
               </div>
+    
             </div>
-          </div>
-
-          <div class="row">
-            <div class="col">
-              <div class="form-group">
-                <label for="email" class="text-muted">Email</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter your email">
-              </div>
-            </div>
-          </div>
-
+    
+          <button class="card-footer btn btn-primary" type="submit">
+            Save Changes
+          </button>
+          </form>
+          
         </div>
+      </div>
 
-      <button class="card-footer btn btn-primary" type="submit">
-        Save Changes
-      </button>
-      </form>
-      
     </div>
 
   </main>
