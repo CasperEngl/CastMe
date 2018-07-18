32a9a41f54952d363f8d641eaaada249eb7b843b
Web-Koncept <web-koncept@WKT-Caspers-iMac.local>
2018-07-18 10:00:20 +0200

    Changed navbar container to be fluid and added mobile support

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -21,48 +21,54 @@
 
 <header>
   <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-    <div class="container">
+    <div class="container-fluid">
       <a class="navbar-brand" href="/">
         <img src="{{ asset('img/logo.png') }}" alt="castme logo">
       </a>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a href="/profile/id" class="nav-link">
-            {{ Auth::User()->name }} | {{ Auth::User()->role }}
-          </a>
-        </li>
-        <li class="nav-item dropdown">
-          <a href="#!" class="nav-link dropdown-toggle" data-toggle="dropdown">
-            <i class="material-icons">notifications</i>
-            <span class="badge">2</span>
-          </a>
-          <div class="dropdown-menu">
-            <a href="#!" class="dropdown-item">
-              <i class="material-icons green lighten-1">local_offer</i> New Post
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="/profile/id" class="nav-link">
+              {{ Auth::User()->name }} | {{ Auth::User()->role }}
             </a>
-            <a href="#!" class="dropdown-item">
-              <i class="material-icons blue lighten-1">question_answer</i> New Message
+          </li>
+          <li class="nav-item dropdown">
+            <a href="#!" class="nav-link dropdown-toggle" data-toggle="dropdown">
+              <i class="material-icons">notifications</i>
+              <span class="badge">2</span>
             </a>
-            <a href="#!" class="dropdown-item">
-              <i class="material-icons red lighten-1">message</i> Messages <span class="badge">2</span>
+            <div class="dropdown-menu">
+              <a href="#!" class="dropdown-item">
+                <i class="material-icons green lighten-1">local_offer</i> New Post
+              </a>
+              <a href="#!" class="dropdown-item">
+                <i class="material-icons blue lighten-1">question_answer</i> New Message
+              </a>
+              <a href="#!" class="dropdown-item">
+                <i class="material-icons red lighten-1">message</i> Messages <span class="badge">2</span>
+              </a>
+            </div>
+          </li>
+        </ul>
+      
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/overview">Overview</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/posts">Posts</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/logout">
+              Logout <i class="material-icons">exit_to_app</i>
             </a>
-          </div>
-        </li>
-      </ul>
-    
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="/overview">Overview</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/posts">Posts</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/logout">
-            Logout <i class="material-icons">exit_to_app</i>
-          </a>
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 </header>
