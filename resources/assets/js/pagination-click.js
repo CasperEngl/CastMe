088a47f8779f768ae98d377e23099bbe3d3efc1c
Casper Engelmann <me@casperengelmann.com>
2018-07-19 23:44:11 +0200

    Supports checkboxes now

--- resources/assets/js/pagination-click.js
+++ resources/assets/js/pagination-click.js
@@ -1,4 +1,18 @@
 $(document).on('click', '.page-link', (e) => {
-	$(e.target).parent().siblings().removeClass('active');
-	$(e.target).parent().addClass('active');
+	const forId = $(e.target).attr('for');
+	const hiddenInput = $('#' + forId).attr('type');
+
+	if (hiddenInput === 'radio') {
+		$(e.target).parent().siblings().removeClass('active');
+		$(e.target).parent().addClass('active');
+	}
+
+	if (hiddenInput === 'checkbox') {
+		console.log($(e.target).parent().hasClass('active'));
+		if ($(e.target).parent().hasClass('active')) {
+			$(e.target).parent().removeClass('active');
+		} else {
+			$(e.target).parent().addClass('active');
+		}
+	}
 });
