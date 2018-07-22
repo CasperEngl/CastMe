0ff241ddaa2a391423a595b9ca3dfc60593a7d2b
Casper Engelmann <me@casperengelmann.com>
2018-07-22 23:05:27 +0200

    Return view for specific user

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class ProfileController extends Controller
 {
     public function index()
     {
         return view('profile');
+    }
+
+    public function user($id) 
+    {
+        return view('user')->with('user', User::find($id));
     }
 
     public function update(Request $request)
