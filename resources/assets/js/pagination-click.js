ed1f3254dec2ab60d2a46c1fb08b4cfd488323c6
Casper Engelmann <me@casperengelmann.com>
2018-07-22 22:19:15 +0200

    Will place active classes on paginations depending on input checked

--- resources/assets/js/pagination-click.js
+++ resources/assets/js/pagination-click.js
@@ -1,3 +1,4 @@
+/*
 $(document).on('click', '.page-link', (e) => {
 	const forId = $(e.target).attr('for');
 	const input = $('#' + forId).attr('type');
@@ -14,4 +15,46 @@ $(document).on('click', '.page-link', (e) => {
 			$(e.target).parent().addClass('active');
 		}
 	}
+});
+*/
+
+$.map($('input:checked'), input => {
+  $('[for^="' + input.id + '"]')
+    .parent()
+    .addClass('active');
+});
+
+$('input').change(function() {
+  // Set variable for the 'this' input for easy usage
+  const input = $(this);
+  // Find the corresponding label
+  const label = $('[for^="' + this.id + '"]');
+
+  // If input is a radio
+  if (input.is(':radio')) {
+    label
+      .parent()
+      .siblings()
+      .removeClass('active');
+		label
+			.parent()
+			.addClass('active');
+  }
+
+  // If input is a checkbox
+  if (input.is(':checkbox')) {
+    // If the pagination is already active
+    if (label.parent().hasClass('active')) {
+      // Remove the active class
+			label
+				.parent()
+				.removeClass('active');
+    } else {
+      // If the pagination is not already active
+      // Add the active class
+			label
+				.parent()
+				.addClass('active');
+    }
+  }
 });
