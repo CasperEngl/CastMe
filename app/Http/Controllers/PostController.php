59019cdd02ad3504673bbeb543e1d6627bed76df
Casper Engelmann <me@casperengelmann.com>
2018-08-01 14:52:23 +0200

    Added new and send functions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,4 +9,12 @@ class PostController extends Controller {
   public function list() {
     return view('posts')->with('posts', Post::orderBy('id', 'desc')->get());
   }
+
+  public function new() {
+    return view('post');
+  }
+
+  public function send() {
+    
+  }
 }
