b89016d2fd9244a4f0a77ace645dfc05eed70f7c
Casper Engelmann <me@casperengelmann.com>
2018-08-02 13:59:17 +0200

    Added New and Edit post page routes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,12 +6,26 @@ use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller {
-  public function list() {
-    return view('posts')->with('posts', Post::orderBy('id', 'desc')->get());
+  public function index() {
+    return view('post');
   }
 
   public function new() {
-    return view('post');
+    return view('post')->with([
+      'title' => __('New post'),
+      'post' => new Post,
+    ]);
+  }
+
+  public function edit($id) {
+    return view('post')->with([
+      'title' => __('Edit post'),
+      'post' => Post::find($id),
+    ]);
+  }
+
+  public function list() {
+    return view('posts')->with('posts', Post::orderBy('id', 'desc')->get());
   }
 
   public function send() {
