deeb8d3bc852288bd8393de87a0a6479f8199135
Casper Engelmann <me@casperengelmann.com>
2018-08-07 17:46:33 +0200

    Renamed post blade to post-build and lowercased localilzation

---
+++ resources/views/post-build.blade.php
@@ -0,0 +1,97 @@
+@extends('master')
+@section('content')
+  <main class="container">
+    <h2 class="page-header">{{ title_case($title) }}</h2>
+
+    <form action="/post/{{ $type }}" method="POST">
+
+      <div class="card">
+        <div class="card-header">
+          <div class="row">
+            <div class="col-auto">
+              <span class="h3">{{ title_case(__('Title')) }}</span>
+            </div>
+            <div class="col">
+              <input type="text" name="title" class="form-control bg-transparent" value="{{ $post->title }}">
+            </div>
+          </div>
+        </div>
+        <div class="card-block">
+          <div class="form-group">
+            <h5 class="text-muted">{{ title_case(__('Looking for')) }}</h5>
+            <p>{{ __('Select multiple if applicable') }}</p>
+            <ul class="pagination">
+              <div class="display-none">
+                <input type="checkbox" name="actor" value="1" id="profile_type-actor">
+                <input type="checkbox" name="dancer" value="1" id="profile_type-dancer">
+                <input type="checkbox" name="entertainer" value="1" id="profile_type-entertainer">
+                <input type="checkbox" name="event_staff" value="1" id="profile_type-event_staff">
+                <input type="checkbox" name="extra" value="1" id="profile_type-extra">
+                <input type="checkbox" name="model" value="1" id="profile_type-model">
+                <input type="checkbox" name="musician" value="1" id="profile_type-musician">
+              </div>
+              <li class="page-item">
+                <label for="profile_type-actor" class="page-link" href="#">{{ title_case(__('Actor')) }}</label>
+              </li>
+              <li class="page-item">
+                <label for="profile_type-dancer" class="page-link" href="#">{{ title_case(__('Dancer')) }}</label>
+              </li>
+              <li class="page-item">
+                <label for="profile_type-entertainer" class="page-link" href="#">{{ title_case(__('Entertainer')) }}</label>
+              </li>
+              <li class="page-item">
+                <label for="profile_type-event_staff" class="page-link" href="#">{{ title_case(__('Event staff')) }}</label>
+              </li>
+              <li class="page-item">
+                <label for="profile_type-extra" class="page-link" href="#">{{ title_case(__('Extra')) }}</label>
+              </li>
+              <li class="page-item">
+                <label for="profile_type-model" class="page-link" href="#">{{ title_case(__('Model')) }}</label>
+              </li>
+              <li class="page-item">
+                <label for="profile_type-musician" class="page-link" href="#">{{ title_case(__('Musician')) }}</label>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="card-block">
+
+          <div class="row mb-2">
+            <div class="col-auto d-flex align-items-center">
+              <h5 class="text-muted m-0">{{ title_case(__('Images')) }}</h5>
+            </div>
+            <div class="col">
+              <a href="#" class="btn btn-primary circle duplicate-input-group-button"><i class="fas fa-plus"></i></a>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="row">
+              <div class="col">
+                <input type="url" name="image[]" placeholder="https://i.imgur.com/XXxXxxX.png" class="form-control text-muted" id="image-1">
+                <div class="valid-feedback feedback-icon">
+                  <i class="fa fa-check"></i>
+               </div>
+               <div class="invalid-feedback feedback-icon">
+                  <i class="fa fa-times"></i>
+               </div>
+              </div>
+              <div class="col-auto">
+                <a href="#" class="btn btn-primary circle remove-input-group-button"><i class="fas fa-minus"></i></a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="card-block">
+          <textarea name="message" class="tinymce">{{ $post->content }}</textarea>
+        </div>
+        <div class="card-footer">
+          <button type="submit" class="btn btn-primary">{{ title_case($type) }}</button>
+        </div>
+      </div>
+
+      {{ csrf_field() }}
+    </form>
+
+  </main>
+@endsection
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,97 +1,34 @@
 @extends('master')
 @section('content')
-  <main class="container">
-    <h2 class="page-header">{{ title_case($title) }}</h2>
+<main class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
 
-    <form action="/post/{{ $type }}" method="POST">
-
+      {{ $post }}
+      
       <div class="card">
         <div class="card-header">
-          <div class="row">
-            <div class="col-auto">
-              <span class="h3">{{ title_case(__('Title')) }}</span>
-            </div>
-            <div class="col">
-              <input type="text" name="title" class="form-control bg-transparent" value="{{ $post->title }}">
-            </div>
-          </div>
+          <h4>{{ title_case($post->title) }}</h4>
         </div>
-        <div class="card-block">
-          <div class="form-group">
-            <h5 class="text-muted">{{ title_case(__('Looking for')) }}</h5>
-            <p>{{ __('Select multiple if applicable') }}</p>
-            <ul class="pagination">
-              <div class="display-none">
-                <input type="checkbox" name="actor" value="1" id="profile_type-actor">
-                <input type="checkbox" name="dancer" value="1" id="profile_type-dancer">
-                <input type="checkbox" name="entertainer" value="1" id="profile_type-entertainer">
-                <input type="checkbox" name="event_staff" value="1" id="profile_type-event_staff">
-                <input type="checkbox" name="extra" value="1" id="profile_type-extra">
-                <input type="checkbox" name="model" value="1" id="profile_type-model">
-                <input type="checkbox" name="musician" value="1" id="profile_type-musician">
-              </div>
-              <li class="page-item">
-                <label for="profile_type-actor" class="page-link" href="#">{{ title_case(__('Actor')) }}</label>
-              </li>
-              <li class="page-item">
-                <label for="profile_type-dancer" class="page-link" href="#">{{ title_case(__('Dancer')) }}</label>
-              </li>
-              <li class="page-item">
-                <label for="profile_type-entertainer" class="page-link" href="#">{{ title_case(__('Entertainer')) }}</label>
-              </li>
-              <li class="page-item">
-                <label for="profile_type-event_staff" class="page-link" href="#">{{ title_case(__('Event staff')) }}</label>
-              </li>
-              <li class="page-item">
-                <label for="profile_type-extra" class="page-link" href="#">{{ title_case(__('Extra')) }}</label>
-              </li>
-              <li class="page-item">
-                <label for="profile_type-model" class="page-link" href="#">{{ title_case(__('Model')) }}</label>
-              </li>
-              <li class="page-item">
-                <label for="profile_type-musician" class="page-link" href="#">{{ title_case(__('Musician')) }}</label>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="card-block">
+        <div class="card-body">
+          <h5 class="text-muted">{{ title_case(__('written by')) }} {{ $post->owner->name }}</h5>
 
-          <div class="row mb-2">
-            <div class="col-auto d-flex align-items-center">
-              <h5 class="text-muted m-0">{{ title_case(__('Images')) }}</h5>
-            </div>
-            <div class="col">
-              <a href="#" class="btn btn-primary circle duplicate-input-group-button"><i class="fas fa-plus"></i></a>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="row">
-              <div class="col">
-                <input type="url" name="image[]" placeholder="https://i.imgur.com/XXxXxxX.png" class="form-control text-muted" id="image-1">
-                <div class="valid-feedback feedback-icon">
-                  <i class="fa fa-check"></i>
-               </div>
-               <div class="invalid-feedback feedback-icon">
-                  <i class="fa fa-times"></i>
-               </div>
-              </div>
-              <div class="col-auto">
-                <a href="#" class="btn btn-primary circle remove-input-group-button"><i class="fas fa-minus"></i></a>
-              </div>
-            </div>
+          <div class="mt-4 mb-4">
+            {!! $post->content !!}
           </div>
 
-        </div>
-        <div class="card-block">
-          <textarea name="message" class="tinymce">{{ $post->content }}</textarea>
+          <h6 class="text-muted">{{ title_case(__('images')) }}</h6>
+          @foreach (json_decode($post->images) as $image)
+            <a href="{{ $image }}" class="image-link" target="_blank">{{ preg_replace('/https|http|(:\/\/)|www\.|\/([^\/]*).*$/', '', $image) }}</a>
+          @endforeach
         </div>
         <div class="card-footer">
-          <button type="submit" class="btn btn-primary">{{ title_case($type) }}</button>
+          <a href="/conversations/new/{{ $post->user_id }}" class="card-link btn btn-primary">{{ title_case(__('respond')) }}</a>
+          <a href="/poke/{{ $post->user_id }}" class="card-link">{{ title_case(__('poke')) }}</a>
         </div>
       </div>
 
-      {{ csrf_field() }}
-    </form>
-
-  </main>
+    </div>
+  </div>
+</main>
 @endsection
