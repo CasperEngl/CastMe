5d093dfcc76c228d38c38ec4aa1fce8d9ca5a28f
Casper Engelmann <me@casperengelmann.com>
2018-08-07 18:23:01 +0200

    Added edit button if user is the owner and display images title if image

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -17,14 +17,25 @@
             {!! $post->content !!}
           </div>
 
-          <h6 class="text-muted">{{ title_case(__('images')) }}</h6>
+          @if (json_decode($post->images)[0] !== null)
+            <h6 class="text-muted">{{ title_case(__('images')) }}</h6>
+          @endif
           @foreach (json_decode($post->images) as $image)
             <a href="{{ $image }}" class="image-link" target="_blank">{{ preg_replace('/https|http|(:\/\/)|www\.|\/([^\/]*).*$/', '', $image) }}</a>
           @endforeach
         </div>
         <div class="card-footer">
-          <a href="/conversations/new/{{ $post->user_id }}" class="card-link btn btn-primary">{{ title_case(__('respond')) }}</a>
-          <a href="/poke/{{ $post->user_id }}" class="card-link">{{ title_case(__('poke')) }}</a>
+          <div class="row justify-content-between">
+            <div class="col-auto">
+              <a href="/conversations/new/{{ $post->user_id }}" class="card-link btn btn-primary">{{ title_case(__('respond')) }}</a>
+              <a href="/poke/{{ $post->user_id }}" class="card-link">{{ title_case(__('poke')) }}</a>
+            </div>
+            @if (Auth::user()->id === $post->user_id)
+            <div class="col-auto">
+              <a href="/post/{{ $post->id }}/edit" class="card-link btn btn-success">{{ title_case(__('edit')) }}</a>
+            </div>
+            @endif
+          </div>
         </div>
       </div>
 
