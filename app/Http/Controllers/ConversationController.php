9e83164e4d807f3601c4c7b8c5b2bc19df546d0b
Casper Engelmann <me@casperengelmann.com>
2018-08-08 23:38:37 +0200

    Send conversation id with Conversation@index

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -6,10 +6,18 @@ use Illuminate\Http\Request;
 
 class ConversationController extends Controller {
   public function index($id) {
-    return view('conversation.singular');
+    return view('conversation.singular')->with([
+      'form_url' => route('conversation.send', [
+        'id' => $id,
+      ]),
+    ]);
   }
 
   public function list() {
     return view('conversation.list');
+  }
+
+  public function send(Request $request) {
+    dd($request->all());
   }
 }
