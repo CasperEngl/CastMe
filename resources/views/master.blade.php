fbf02a802f9fbdb7ebe7dd864f7dea6fd54cce94
Casper Engelmann <me@casperengelmann.com>
2018-08-08 20:57:22 +0200

    Extend master instead of layout.app

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -43,18 +43,37 @@
             </div>
           </li>
         </ul>
-      
+
         <ul class="navbar-nav ml-auto">
+          @guest
           <li class="nav-item">
-            <a class="nav-link" href="/overview">{{ title_case(__('overview')) }}</a>
+            <a class="nav-link" href="{{ route('login') }}">{{ title_case(__('login')) }}</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/posts">{{ title_case(__('posts')) }}</a>
+            <a class="nav-link" href="{{ route('register') }}">{{ title_case(__('register')) }}</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/logout">{{ title_case(__('logout')) }} <i class="fas fa-sign-out-alt"></i></a>
+          @else
+          <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+              aria-expanded="false" v-pre>
+              {{ Auth::user()->name }} <span class="caret"></span>
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="{{ route('overview') }}">{{ title_case(__('overview')) }}</a>
+              <a class="dropdown-item" href="{{ route('posts') }}">{{ title_case(__('posts')) }}</a>
+              <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                {{ title_case(__('logout')) }} <i class="fas fa-sign-out-alt"></i>
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+              </form>
+            </div>
           </li>
+          @endguest
         </ul>
+
       </div>
     </div>
   </nav>
