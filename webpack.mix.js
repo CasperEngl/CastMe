f40e0ec7adb37f25be85d88a9d7d34070bcf2172
Casper Engelmann <me@casperengelmann.com>
2018-08-13 14:59:06 +0200

    Changed let to const

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -16,5 +16,5 @@ mix
   .sass('resources/assets/sass/app.scss', 'public/css');
 
 if (mix.inProduction()) {
-    mix.version();
+  mix.version();
 }
