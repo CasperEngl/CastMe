1da9617f0f75005c2c56531f1f566f10ad4abbb3
Casper Engelmann <me@casperengelmann.com>
2018-08-15 09:39:34 +0200

    Removed btn-primary from tiles

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -4,22 +4,22 @@
     <h2 class="page-header">{{ title_case(__('options')) }}</h2>
     <div class="row">
       <div class="col-12 col-sm-6 col-md-3">
-        <a href="{{ route('user.subscription') }}" class="btn btn-primary tile">
+        <a href="{{ route('user.subscription') }}" class="btn tile">
           <i class="fas fa-grin"></i> {{ title_case(__('subscription')) }}
         </a>
       </div>
       <div class="col-12 col-sm-6 col-md-3">
-        <a href="{{ route('user.settings') }}" class="btn btn-primary tile">
+        <a href="{{ route('user.settings') }}" class="btn tile">
           <i class="fas fa-user"></i> {{ title_case(__('profile settings')) }}
         </a>
       </div>
       <div class="col-12 col-sm-6 col-md-3">
-        <a href="{{ route('posts') }}" class="btn btn-primary tile">
+        <a href="{{ route('posts') }}" class="btn tile">
           <i class="fas fa-bookmark"></i> {{ title_case(__('posts')) }}
         </a>
       </div>
       <div class="col-12 col-sm-6 col-md-3">
-        <a href="{{ route('conversations') }}" class="btn btn-primary tile">
+        <a href="{{ route('conversations') }}" class="btn tile">
           <i class="fas fa-comments"></i> {{ title_case(__('conversations')) }}
         </a>
       </div>
@@ -29,7 +29,7 @@
       <h2 class="page-header">{{ title_case(__('scout')) }}</h2>
       <div class="row">
         <div class="col-12 col-sm-6 col-md-3">
-          <a href="{{ route('post.new') }}" class="btn btn-primary tile">
+          <a href="{{ route('post.new') }}" class="btn tile">
             <i class="fas fa-plus"></i> {{ title_case(__('add post')) }}
           </a>
         </div>
@@ -40,7 +40,7 @@
       <h2 class="page-header">{{ title_case(__('admin')) }}</h2>
       <div class="row">
         <div class="col-12 col-sm-6 col-md-3">
-          <a href="#" class="btn btn-primary tile">
+          <a href="#" class="btn tile">
             <i class="fas fa-users"></i> {{ title_case(__('users')) }}
           </a>
         </div>
