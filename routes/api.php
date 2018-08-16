d69f5a527f316692d1102458148d5fb525ec93a3
Casper Engelmann <me@casperengelmann.com>
2018-08-16 15:31:10 +0200

    Added web middleware

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('locale/{locale?}', 'LocaleController@get')->name('locale.get');
+Route::group(['middleware' => ['web']], function () {
+    Route::get('locale/{locale?}', 'LocaleController@get')->name('locale.get');
+});
