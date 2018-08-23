089424f36797fd054303192e5b6ed3ec6ca35e42
Casper Engelmann <me@casperengelmann.com>
2018-08-23 04:19:33 +0200

    Added white background and used sections for rows instead of divs

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.master')
 @section('content')
-  <main class="container">
+  <main class="container bg-white rounded px-5">
     <h2 class="page-header">{{ title_case(__('options')) }}</h2>
-    <div class="row">
+    <section class="row">
       <div class="col-12 col-sm-6 col-md-3">
         <a href="{{ route('user.subscription') }}" class="btn tile">
           <i class="fas fa-grin"></i> {{ title_case(__('subscription')) }}
@@ -23,28 +23,28 @@
           <i class="fas fa-comments"></i> {{ title_case(__('conversations')) }}
         </a>
       </div>
-    </div>
+    </section>
     {{--Scout only--}}
     @if(in_array(Auth::user()->role, ['Scout', 'Moderator', 'Admin']))
-      <h2 class="page-header">{{ title_case(__('scout')) }}</h2>
-      <div class="row">
-        <div class="col-12 col-sm-6 col-md-3">
-          <a href="{{ route('post.new') }}" class="btn tile">
-            <i class="fas fa-plus"></i> {{ title_case(__('new post')) }}
-          </a>
-        </div>
+    <section class="page-header">{{ title_case(__('scout')) }}</section>
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-3">
+        <a href="{{ route('post.new') }}" class="btn tile">
+          <i class="fas fa-plus"></i> {{ title_case(__('new post')) }}
+        </a>
       </div>
+    </div>
     @endif
     {{--Admin only--}}
     @if(in_array(Auth::user()->role, ['Moderator', 'Admin']))
-      <h2 class="page-header">{{ title_case(__('admin')) }}</h2>
-      <div class="row">
-        <div class="col-12 col-sm-6 col-md-3">
-          <a href="#" class="btn tile">
-            <i class="fas fa-users"></i> {{ title_case(__('users')) }}
-          </a>
-        </div>
+    <h2 class="page-header">{{ title_case(__('admin')) }}</h2>
+    <section class="row">
+      <div class="col-12 col-sm-6 col-md-3">
+        <a href="#" class="btn tile">
+          <i class="fas fa-users"></i> {{ title_case(__('users')) }}
+        </a>
       </div>
+    </section>
     @endif
   </main>
 @endsection
