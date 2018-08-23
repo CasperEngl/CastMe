e47d19eec7839c073255b1f292de2e077f3c8bbe
Casper Engelmann <me@casperengelmann.com>
2018-08-23 02:36:59 +0200

    Added @new conversation control

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Crypt;
 
 class ConversationController extends Controller {
   public function index($id) {
@@ -15,6 +16,15 @@ class ConversationController extends Controller {
 
   public function list() {
     return view('conversation.list');
+  }
+
+  public function new(Request $request) {
+    $user = Crypt::decrypt($request->input('user'));
+
+    return response()->json([
+      'user_crypt' => $request->input('user'),
+      'user' => $user,
+    ]);
   }
 
   public function send(Request $request) {
