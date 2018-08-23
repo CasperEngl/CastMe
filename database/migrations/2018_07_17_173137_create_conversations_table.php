f8bcad64dfe5cb36506982b4ed3e297432af8b3f
Casper Engelmann <me@casperengelmann.com>
2018-08-23 03:45:49 +0200

    Renamed Message to Conversation because with have Reply (New Message)

--- database/migrations/2018_07_17_173137_create_conversations_table.php
+++ database/migrations/2018_07_17_173137_create_conversations_table.php
@@ -4,20 +4,19 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMessagesTable extends Migration {
+class CreateConversationsTable extends Migration {
   /**
    * Run the migrations.
    *
    * @return void
    */
   public function up() {
-    Schema::create('messages', function (Blueprint $table) {
+    Schema::create('conversations', function (Blueprint $table) {
       $table->increments('id');
       $table->integer('sender');
       $table->integer('receiver');
-      $table->string('title');
       $table->text('content');
-      $table->boolean('read');
+      $table->boolean('read')->default(true);
       $table->timestamps();
     });
   }
