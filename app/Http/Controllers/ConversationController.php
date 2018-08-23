49e1b894b2b338b6778dac7d7a5426f1110ef38f
Casper Engelmann <me@casperengelmann.com>
2018-08-23 04:18:46 +0200

    Added send(), new redirects from input user to single conversation

--- app/Http/Controllers/ConversationController.php
+++ app/Http/Controllers/ConversationController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 use Crypt;
+use App\Conversation;
 
 class ConversationController extends Controller {
   public function index($id) {
+    $replies = Conversation::findOrFail($id);
+    return response()->json([
+      'test' => $replies
+    ]);
+
     return view('conversation.singular')->with([
-      'form_url' => route('conversation.send', [
-        'id' => $id,
-      ]),
+      'form_url' => route('conversation.send')
     ]);
   }
 
@@ -19,15 +24,24 @@ class ConversationController extends Controller {
   }
 
   public function new(Request $request) {
-    $user = Crypt::decrypt($request->input('user'));
-
-    return response()->json([
-      'user_crypt' => $request->input('user'),
-      'user' => $user,
+    return redirect()->route('conversation', [
+      'id' => Crypt::decrypt($request->input('user')),
     ]);
   }
 
   public function send(Request $request) {
-    dd($request->all());
+    $user = Crypt::decrypt($request->input('user'));
+
+    $message = new Conversation([
+      'sender' => Auth::id(),
+      'receiver' => $user,
+      'content' => $request->input('content'),
+      'read' => false,
+    ]);
+
+    $message->save();
+
+    Flash::push('success', StringFormat::format(__('your message was sent!')));
+    return redirect()->back();
   }
 }
