ffc96d2da8373a5b5bf34f527ea90b0da01117ec
Casper Engelmann <me@casperengelmann.com>
2018-08-23 02:32:30 +0200

    Assigned comment post_id to Post and comment user_id to User

--- app/Comment.php
+++ app/Comment.php
@@ -10,10 +10,10 @@ class Comment extends Model {
   ];
 
   public function post() {
-    return $this->belongsTo('App\Post');
+    return $this->belongsTo('App\Post', 'post_id');
   }
 
   public function owner() {
-    return $this->belongsTo('App\User');
+    return $this->belongsTo('App\User', 'user_id');
   }
 }
