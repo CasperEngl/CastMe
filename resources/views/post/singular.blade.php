5e56cf9091756b2df71ad8c4a1f9db17bc10de5e
Casper Engelmann <me@casperengelmann.com>
2018-08-27 00:20:35 +0200

    Show user own comments if not owner

--- resources/views/post/singular.blade.php
+++ resources/views/post/singular.blade.php
@@ -25,7 +25,7 @@
           @if (json_decode($post->images)[0] !== null)
             <h6 class="text-muted">{{ title_case(__('images')) }}</h6>
             @foreach (json_decode($post->images) as $image)
-            <a href="{{ $image }}" class="image-link" target="_blank">{{ preg_replace('/https|http|(:\/\/)|www\.|\/([^\/]*).*$/', '', $image) }}</a>
+            <a href="{{ $image }}" class="image-link" target="_blank">{{ Format::stripDomain($image) }}</a>
             @endforeach
           @endif
         </div>
@@ -44,46 +44,55 @@
         @if (count($comments) > 0)
           <h2 class="page-header mb-0">{{ title_case(__('comments')) }}</h2>
           @foreach ($comments as $comment)
-            {{-- Output comment if user is post owner or user own comment --}}
-            @if ($owner || Auth::id() === $comment->user_id)
-            <div class="card my-3">
-              {{ Auth::id() === $comment->owner }}
-              <div class="card-header">{{ $comment->owner->name }} {{ $comment->owner->last_name }}</div>
-              <div class="card-body">
-                {{ strip_tags($comment->content) }}
-              </div>
-              <div class="card-footer">
-                <div class="row align-items-center">
-                  <div class="col">{{ Carbon::parse($comment->updated_at)->format('M j \a\t G:i') }}</div>
-                  <div class="col-auto">
-                    <form action="{{ route('conversation.new') }}" method="POST">
-                      <button class="btn btn-primary" type="submit">{{ ucfirst(__('message')) }}</button>
-
-                      {{ Form::hidden('user', $comment->owner->id) }}
-                      @csrf
-                      @method('POST')
-                    </form>
-                  </div>
+          <div class="card my-3">
+            {{ Auth::id() === $comment->owner }}
+            <div class="card-header">{{ $comment->owner->name }} {{ $comment->owner->last_name }}</div>
+            <div class="card-body">
+              {{ strip_tags($comment->content) }}
+            </div>
+            <div class="card-footer">
+              <div class="row align-items-center">
+                <div class="col">{{ Carbon::parse($comment->updated_at)->format('M j \a\t G:i') }}</div>
+                <div class="col-auto">
+                  <a href="{{ route('conversation', ['id' => $comment->user_id]) }}" class="btn btn-primary">{{ ucfirst(__('message')) }}</a>
                 </div>
               </div>
             </div>
-            @endif
+          </div>
           @endforeach
-        {{-- If no comments are found --}}
         @else
+        {{-- If nocomments are found --}}
         <h2 class="page-header mb-0">{{ title_case(__('No comments')) }}</h2>
         @endif
-      @endif
-      @if (!$owner)
-      <form action="{{ route('comment.new') }}" method="POST">
-        <h2 class="page-header mb-0">{{ ucfirst(__('comment')) }}</h2>
-        <textarea name="content" class="tinymce"></textarea>
-        <button class="btn btn-primary" type="submit">{{ title_case(__('message')) }}</button>
+      @else
+        @foreach ($comments as $comment)
+          @if (Auth::id() === $comment->user_id)
+          <div class="card my-3">
+            {{ Auth::id() === $comment->owner }}
+            <div class="card-header">{{ strtoupper(__('you')) }}</div>
+            <div class="card-body">
+              {{ strip_tags($comment->content) }}
+            </div>
+            <div class="card-footer">
+              <div class="row align-items-center">
+                <div class="col">{{ Carbon::parse($comment->updated_at)->format('M j \a\t G:i') }}</div>
+                <div class="col-auto">
+                  <a href="{{ route('conversation', ['id' => $comment->user_id]) }}" class="btn btn-primary">{{ ucfirst(__('message')) }}</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          @endif
+        @endforeach
+        <form action="{{ route('comment.new') }}" method="POST">
+          <h2 class="page-header mb-0">{{ ucfirst(__('comment')) }}</h2>
+          <textarea name="content" class="tinymce"></textarea>
+          <button class="btn btn-primary" type="submit">{{ title_case(__('comment')) }}</button>
 
-        {{ Form::hidden('post', $post->id) }}
-        @csrf
-        @method('POST')
-      </form>
+          {{ Form::hidden('post', $post->id) }}
+          @csrf
+          @method('POST')
+        </form>
       @endif
 
     </div>
