67968a66d6fcfe87d4e6d85a5a1b2a8d6b8e1ce7
Casper Engelmann <me@casperengelmann.com>
2018-08-27 01:27:45 +0200

    Changed columns

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -5,24 +5,24 @@
       <div class="col-12">
         <h2 class="page-header">{{ title_case(__('options')) }}</h2>
       </div>
-      <div class="col-12 col-sm-6 col-md-3">
+      <div class="col-12 col-sm-6 col-lg-4">
         <a href="{{ route('user.subscription') }}" class="btn tile">
-          <i class="fas fa-grin"></i> {{ title_case(__('subscription')) }}
+          <i class="fas fa-grin"></i><span class="d-inline-block">{{ title_case(__('subscription')) }}</span>
         </a>
       </div>
-      <div class="col-12 col-sm-6 col-md-3">
+      <div class="col-12 col-sm-6 col-lg-4">
         <a href="{{ route('user.settings') }}" class="btn tile">
-          <i class="fas fa-user"></i> {{ title_case(__('profile settings')) }}
+          <i class="fas fa-user"></i><span class="d-inline-block">{{ title_case(__('profile settings')) }}</span>
         </a>
       </div>
-      <div class="col-12 col-sm-6 col-md-3">
+      <div class="col-12 col-sm-6 col-lg-4">
         <a href="{{ route('posts') }}" class="btn tile">
-          <i class="fas fa-bookmark"></i> {{ title_case(__('posts')) }}
+          <i class="fas fa-bookmark"></i><span class="d-inline-block">{{ title_case(__('posts')) }}</span>
         </a>
       </div>
-      <div class="col-12 col-sm-6 col-md-3">
+      <div class="col-12 col-sm-6 col-lg-4">
         <a href="{{ route('conversations') }}" class="btn tile">
-          <i class="fas fa-comments"></i> {{ title_case(__('conversations')) }}
+          <i class="fas fa-comments"></i><span class="d-inline-block">{{ title_case(__('conversations')) }}</span>
         </a>
       </div>
     </section>
@@ -32,9 +32,9 @@
       <div class="col-12">
         <h2 class="page-header">{{ title_case(__('scout')) }}</h2>
       </div>
-      <div class="col-12 col-sm-6 col-md-3">
+      <div class="col-12 col-sm-6 col-lg-4">
         <a href="{{ route('post.new') }}" class="btn tile">
-          <i class="fas fa-plus"></i> {{ title_case(__('new post')) }}
+          <i class="fas fa-plus"></i><span class="d-inline-block">{{ title_case(__('new post')) }}</span>
         </a>
       </div>
     </section>
@@ -45,9 +45,9 @@
       <div class="col-12">
         <h2 class="page-header">{{ title_case(__('admin')) }}</h2>
       </div>
-      <div class="col-12 col-sm-6 col-md-3">
+      <div class="col-12 col-sm-6 col-lg-4">
         <a href="#" class="btn tile">
-          <i class="fas fa-users"></i> {{ title_case(__('users')) }}
+          <i class="fas fa-users"></i><span class="d-inline-block">{{ title_case(__('users')) }}</span>
         </a>
       </div>
     </section>
