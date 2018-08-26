3e324b85d51ba1923d649ce3fa64d5b9f92b3291
Casper Engelmann <me@casperengelmann.com>
2018-08-26 22:28:28 +0200

    Put h2's inside the rows

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.master')
 @section('content')
   <main class="container bg-white rounded px-5">
-    <h2 class="page-header">{{ title_case(__('options')) }}</h2>
     <section class="row">
+      <div class="col-12">
+        <h2 class="page-header">{{ title_case(__('options')) }}</h2>
+      </div>
       <div class="col-12 col-sm-6 col-md-3">
         <a href="{{ route('user.subscription') }}" class="btn tile">
           <i class="fas fa-grin"></i> {{ title_case(__('subscription')) }}
@@ -26,19 +28,23 @@
     </section>
     {{--Scout only--}}
     @if(in_array(Auth::user()->role, ['Scout', 'Moderator', 'Admin']))
-    <section class="page-header">{{ title_case(__('scout')) }}</section>
-    <div class="row">
+    <section class="row">
+      <div class="col-12">
+        <h2 class="page-header">{{ title_case(__('scout')) }}</h2>
+      </div>
       <div class="col-12 col-sm-6 col-md-3">
         <a href="{{ route('post.new') }}" class="btn tile">
           <i class="fas fa-plus"></i> {{ title_case(__('new post')) }}
         </a>
       </div>
-    </div>
+    </section>
     @endif
     {{--Admin only--}}
     @if(in_array(Auth::user()->role, ['Moderator', 'Admin']))
-    <h2 class="page-header">{{ title_case(__('admin')) }}</h2>
     <section class="row">
+      <div class="col-12">
+        <h2 class="page-header">{{ title_case(__('admin')) }}</h2>
+      </div>
       <div class="col-12 col-sm-6 col-md-3">
         <a href="#" class="btn tile">
           <i class="fas fa-users"></i> {{ title_case(__('users')) }}
