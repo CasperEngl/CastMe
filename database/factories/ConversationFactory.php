e7679cc31d5a8f160bb45ce9a76d403853ab8102
Casper Engelmann <me@casperengelmann.com>
2018-08-27 10:21:56 +0200

    Added _id

--- database/factories/ConversationFactory.php
+++ database/factories/ConversationFactory.php
@@ -5,8 +5,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Conversation::class, function (Faker $faker) {
     return [
-        'sender' => rand(1, 2),
-        'receiver' => rand(1, 2),
+        'sender_id' => rand(1, 2),
+        'receiver_id' => rand(1, 2),
         'content' => $faker->paragraph
     ];
 });
