285f774b41bc9369b701bd52bae827bd2f5da966
Casper Engelmann <me@casperengelmann.com>
2018-08-27 10:22:13 +0200

    Added more fake information

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,9 +4,14 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'name' => $faker->firstName,
+        'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'avatar' => $faker->imageUrl(),
         'remember_token' => str_random(10),
+        'stripe_id' => $faker->uuid,
+        'card_brand' => $faker->creditCardType,
+        'card_last_four' => substr($faker->creditCardNumber, -4)
     ];
 });
