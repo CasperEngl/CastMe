d8a4c7dd053640bc847d79ec24d31b8b50d306b6
Casper Engelmann <me@casperengelmann.com>
2018-08-29 00:16:16 +0200

    Added subscription conditions accept check and use back()

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -26,14 +26,25 @@ class SubscriptionController extends Controller {
   public function create(Request $request) {
     $user = Auth::user();
     $model = $request->input('months');
+    $accepted = $request->input('accepth');
 
-    //Checks if user already have a subscription
+    // Make sure user accepted conditions
+    if ($accepted !== 'on')
+      return redirect()->back()->withErrors([
+        ucfirst(__('you need to accept our subscription conditions'))
+      ]);
+
+    // Checks if user already have a subscription
     if ($user->subscribed('paid'))
-      return redirect()->route('user.subscription')->withErrors([ucfirst(__('user already have an active subscription'))]);
+      return redirect()->back()->withErrors([
+        ucfirst(__('user already have an active subscription'))
+      ]);
 
-    //Checks that $request->input('sub') is not manipulated
+    // Checks that $request->input('sub') is not manipulated
     if(!in_array($model, ['2', '3', '6', '12']))
-      return redirect()->route('user.subscription')->withErrors([ucfirst(__('unknown billing model'))]);
+      return redirect()->back()->withErrors([
+        ucfirst(__('unknown billing model'))
+      ]);
 
     $user->newSubscription('paid', $model . '_months')->create($request->input('stripeToken'));
 
@@ -44,14 +55,25 @@ class SubscriptionController extends Controller {
   public function swap(Request $request) {
     $user = Auth::user();
     $model = $request->input('months');
+    $accepted = $request->input('accepth');
+
+    // Make sure user accepted conditions
+    if ($accepted !== 'on')
+      return redirect()->back()->withErrors([
+        ucfirst(__('you need to accept our subscription conditions'))
+      ]);
 
     if (!$user->subscribed('paid'))
-      return redirect()->route('user.subscription')->withErrors([__('user does not have active subscription')]);
+      return redirect()->back()->withErrors([
+        ucfirst(__('user does not have active subscription'))
+        ]);
 
     if(!in_array($model, ['2', '3', '6', '12']))
-      return redirect()->route('user.subscription')->withErrors([__('unknown billing model')]);
+      return redirect()->back()->withErrors([
+        ucfirst(__('unknown billing model'))
+      ]);
 
-    $user->subscription()->swap($model . '_months');
+    $user->subscription('paid')->swap($model . '_months');
 
     Flash::push('success', ucfirst(__('changed billing cycle')));
     return redirect()->route('user.subscription');
