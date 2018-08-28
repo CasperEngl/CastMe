3a4b2b624248aa160c36723884579a17736f9ef0
Casper Engelmann <me@casperengelmann.com>
2018-08-28 02:22:15 +0200

    Hide upload placeholder and upload button

--- resources/assets/js/avatar.js
+++ resources/assets/js/avatar.js
@@ -5,12 +5,12 @@ $('#avatar').fileinput({
   maxFileSize: 2000,
   showClose: false,
   showCaption: false,
+  showUpload: false,
   browseLabel: 'Select file',
   removeLabel: 'Remove file',
   browseIcon: '<i class="fas fa-folder-open"></i>',
   removeIcon: '<i class="fas fa-trash"></i>',
   elErrorContainer: '#avatar-errors',
   msgErrorClass: 'alert alert-block alert-danger',
-  defaultPreviewContent: '<img src="/avatar/avatar-64.png" alt="your avatar">',
   allowedFileExtensions: ['jpg', 'png', 'gif'],
 });
