cb61a66fe0ba671563034d77a95eff5c11303ada
Casper Engelmann <me@casperengelmann.com>
2018-09-06 21:56:11 +0200

    removed row

--- resources/views/user/subscription.blade.php
+++ resources/views/user/subscription.blade.php
@@ -8,155 +8,135 @@ use \Illuminate\Support\Facades\Auth;
 @section('content')
 <h2 class="page-header">{{ title_case(__('subscription')) }}</h2>
 
-<div class="row">
+@if(Auth::user()->activeSub())
+<form action="{{ route('user.subscription.swap') }}" method="POST" id="payment-form">
+@else
+<form action="{{ route('user.subscription.create') }}" method="POST" id="payment-form">
+@endif
+  <div class="card">
+    <div class="card-body">
+      <h3 class="page-header">{{ title_case(__('subscribe')) }}</h3>
 
-  <div class="col-12 col-sm-4">
-    <div class="card">
-      <div class="card-body">
-        <h3 class="card-title mb-0">
-          @if (Auth::user()->activeSub()) 
-            {{ title_case(__('active')) }}
-          @else
-            {{ title_case(__('inactive')) }}
-          @endif
-        </h3>
-      </div>
-    </div>
-  </div>
-  <div class="col-12 col-sm-8">
-    @if(Auth::user()->activeSub())
-    <form action="{{ route('user.subscription.swap') }}" method="POST" id="payment-form">
-    @else
-    <form action="{{ route('user.subscription.create') }}" method="POST" id="payment-form">
-    @endif
-      <div class="card">
-        <div class="card-body">
-          <h3 class="page-header">{{ title_case(__('subscribe')) }}</h3>
-
-          <div class="row">
-            <div class="col-sm-4">
-              <ul class="pagination">
-                <li class="page-item w-100 d-flex justify-content-center">
-                  <label for="month-2" class="page-link w-100 text-center">2 {{ __('months') }} - 179 DKK</label>
-                </li>
-                <li class="page-item w-100 d-flex justify-content-center">
-                  <label for="month-3" class="page-link w-100 text-center">3 {{ __('months') }} - 279 DKK</label>
-                </li>
-                <li class="page-item w-100 d-flex justify-content-center">
-                  <label for="month-6" class="page-link w-100 text-center">6 {{ __('months') }} - 449 DKK</label>
-                </li>
-                <li class="page-item w-100 d-flex justify-content-center">
-                  <label for="month-12" class="page-link w-100 text-center">12 {{ __('months') }} - 799 DKK</label>
-                </li>
-                <div class="display-none">
-                  <input type="radio" name="months" value="2_months" id="month-2" {{ $plan === '2_months' ? 'checked' : '' }}>
-                  <input type="radio" name="months" value="3_months" id="month-3" {{ $plan === '3_months' ? 'checked' : '' }}>
-                  <input type="radio" name="months" value="6_months" id="month-6" {{ $plan === '6_months' ? 'checked' : '' }}>
-                  <input type="radio" name="months" value="12_months" id="month-12" {{ $plan === '12_months' ? 'checked' : '' }}>
-                </div>
-              </ul>
+      <div class="row">
+        <div class="col-sm-4">
+          <ul class="pagination">
+            <li class="page-item w-100 d-flex justify-content-center">
+              <label for="month-2" class="page-link w-100 text-center">2 {{ __('months') }} - 179 DKK</label>
+            </li>
+            <li class="page-item w-100 d-flex justify-content-center">
+              <label for="month-3" class="page-link w-100 text-center">3 {{ __('months') }} - 279 DKK</label>
+            </li>
+            <li class="page-item w-100 d-flex justify-content-center">
+              <label for="month-6" class="page-link w-100 text-center">6 {{ __('months') }} - 449 DKK</label>
+            </li>
+            <li class="page-item w-100 d-flex justify-content-center">
+              <label for="month-12" class="page-link w-100 text-center">12 {{ __('months') }} - 799 DKK</label>
+            </li>
+            <div class="display-none">
+              <input type="radio" name="months" value="2_months" id="month-2" {{ $plan === '2_months' ? 'checked' : '' }}>
+              <input type="radio" name="months" value="3_months" id="month-3" {{ $plan === '3_months' ? 'checked' : '' }}>
+              <input type="radio" name="months" value="6_months" id="month-6" {{ $plan === '6_months' ? 'checked' : '' }}>
+              <input type="radio" name="months" value="12_months" id="month-12" {{ $plan === '12_months' ? 'checked' : '' }}>
             </div>
-          
-            <div class="col-sm-8">
-              <script src="https://js.stripe.com/v3/"></script>
-
-              <div id="card-element" class="my-4 field is-empty"></div>
-              <div id="card-errors"></div>
-
-              <script>
-                const stripeTokenHandler = token => {
-                  // Insert the token ID into the form so it gets submitted to the server
-                  const form = document.getElementById('payment-form');
-                  const hiddenInput = document.createElement('input');
-                  hiddenInput.setAttribute('type', 'hidden');
-                  hiddenInput.setAttribute('name', 'stripeToken');
-                  hiddenInput.setAttribute('value', token.id);
-                  form.appendChild(hiddenInput);
-
-                  // Submit the form
-                  form.submit();
-                };
-
-                let stripe = Stripe('{{ env('STRIPE_KEY') }}');
-                let elements = stripe.elements();
-
-                let card = elements.create('card', {
-                  hidePostalCode: true,
-                  iconStyle: 'solid',
-                  style: {
-                    base: {
-                      iconColor: '#8898AA',
-                      color: 'rgba(0, 0, 0, .54)',
-                      lineHeight: '36px',
-                      fontWeight: 400,
-                      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-                      fontSize: '1.25rem',
-
-                      '::placeholder': {
-                        color: '#8898AA'
-                      }
-                    },
-                    invalid: {
-                      iconColor: '#e85746',
-                      color: '#e85746'
-                    }
-                  },
-                  classes: {
-                    focus: 'is-focused',
-                    empty: 'is-empty'
-                  }
-                });
-                card.mount('#card-element');
-
-                card.addEventListener('change', ({ error }) => {
-                  const displayError = document.getElementById('card-errors');
-                  if (error) {
-                    displayError.textContent = error.message;
-                  } else {
-                    displayError.textContent = "";
-                  }
-                });
-
-                // Create a token or display an error when the form is submitted.
-                const form = document.getElementById('payment-form');
-                form.addEventListener('submit', async event => {
-                  event.preventDefault();
-
-                  const { token, error } = await stripe.createToken(card);
-
-                  if (error) {
-                    // Inform the customer that there was an error.
-                    const errorElement = document.getElementById('card-errors');
-                    errorElement.textContent = error.message;
-                  } else {
-                    // Send the token to your server.
-                    stripeTokenHandler(token);
-                  }
-                });
-              </script>
-
-              <input type="checkbox" name="accepth" id="accepth">
-              <label for="accepth">{{ ucfirst(__('i accept the')) }} <a href="https://castme.dk/abonnementsbetingelser/" target="blank">{{
-                  __('subscription conditions') }}</a></label>
-            </div>
-          </div>
-
+          </ul>
         </div>
+      
+        <div class="col-sm-8">
+          <script src="https://js.stripe.com/v3/"></script>
 
-        <button class="card-footer btn btn-primary" type="submit">
-          @if(Auth::user()->activeSub())
-            {{ title_case(__('update information')) }}
-          @else
-            {{ title_case(__('subscribe')) }}
-          @endif
-        </button>
+          <div id="card-element" class="my-4 field is-empty"></div>
+          <div id="card-errors"></div>
+
+          <script>
+            const stripeTokenHandler = token => {
+              // Insert the token ID into the form so it gets submitted to the server
+              const form = document.getElementById('payment-form');
+              const hiddenInput = document.createElement('input');
+              hiddenInput.setAttribute('type', 'hidden');
+              hiddenInput.setAttribute('name', 'stripeToken');
+              hiddenInput.setAttribute('value', token.id);
+              form.appendChild(hiddenInput);
+
+              // Submit the form
+              form.submit();
+            };
+
+            let stripe = Stripe('{{ env('STRIPE_KEY') }}');
+            let elements = stripe.elements();
+
+            let card = elements.create('card', {
+              hidePostalCode: true,
+              iconStyle: 'solid',
+              style: {
+                base: {
+                  iconColor: '#8898AA',
+                  color: 'rgba(0, 0, 0, .54)',
+                  lineHeight: '36px',
+                  fontWeight: 400,
+                  fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+                  fontSize: '1.25rem',
+
+                  '::placeholder': {
+                    color: '#8898AA'
+                  }
+                },
+                invalid: {
+                  iconColor: '#e85746',
+                  color: '#e85746'
+                }
+              },
+              classes: {
+                focus: 'is-focused',
+                empty: 'is-empty'
+              }
+            });
+            card.mount('#card-element');
+
+            card.addEventListener('change', ({ error }) => {
+              const displayError = document.getElementById('card-errors');
+              if (error) {
+                displayError.textContent = error.message;
+              } else {
+                displayError.textContent = "";
+              }
+            });
+
+            // Create a token or display an error when the form is submitted.
+            const form = document.getElementById('payment-form');
+            form.addEventListener('submit', async event => {
+              event.preventDefault();
+
+              const { token, error } = await stripe.createToken(card);
+
+              if (error) {
+                // Inform the customer that there was an error.
+                const errorElement = document.getElementById('card-errors');
+                errorElement.textContent = error.message;
+              } else {
+                // Send the token to your server.
+                stripeTokenHandler(token);
+              }
+            });
+          </script>
+
+          <input type="checkbox" name="accepth" id="accepth">
+          <label for="accepth">{{ ucfirst(__('i accept the')) }} <a href="https://castme.dk/abonnementsbetingelser/" target="blank">{{
+              __('subscription conditions') }}</a></label>
+        </div>
       </div>
 
-      @csrf
-      @method('POST')
-    </form>
-    
+    </div>
+
+    <button class="card-footer btn btn-primary" type="submit">
+      @if(Auth::user()->activeSub())
+        {{ title_case(__('update information')) }}
+      @else
+        {{ title_case(__('subscribe')) }}
+      @endif
+    </button>
   </div>
 
-</div>
+  @csrf
+  @method('POST')
+</form>
 @endsection
