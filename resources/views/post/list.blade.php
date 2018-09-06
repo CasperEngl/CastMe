a79f0a02761b93a1254bc405d1a80ee2985523eb
Casper Engelmann <me@casperengelmann.com>
2018-09-06 21:33:46 +0200

    Added post-cards to replace old cards

--- resources/views/post/list.blade.php
+++ resources/views/post/list.blade.php
@@ -1,37 +1,51 @@
 @extends('layouts.master')
 @section('content')
-  <main class="container">
-    @if(in_array(Auth::user()->role, ['Scout', 'Moderator', 'Admin']))
-    <a href="{{ route('post.new') }}" class="btn btn-primary btn-lg mb-4" role="button">{{ ucfirst(__('new post')) }}</a>
-    @endif
+@if(in_array(Auth::user()->role, ['Scout', 'Moderator', 'Admin']))
+<a href="{{ route('post.new') }}" class="btn btn-primary btn-lg mb-4" role="button">{{ ucfirst(__('new post')) }}</a>
+@endif
 
-    <div class="row">
+<div class="row">
 
-      @foreach($posts as $post)
+  @foreach($posts as $post)
+    {{-- 
+      <div class="col-md-4 mt-2 mb-2">
+        <article class="card h-100">
+          <section class="card-body d-flex flex-wrap">
+            <h4 class="card-title w-100">{{ title_case($post->title) }}</h4>
+            @if (Auth::user()->id === $post->user_id)
+              <h6 class="card-subtitle w-100 mb-2 text-muted">{{ ucfirst(__('written by')) }} {{ strtoupper(__('you')) }}</h6>
+            @else
+              <h6 class="card-subtitle w-100 mb-2 text-muted">{{ ucfirst(__('written by')) }} {{ $post->owner->name }}</h6>
+            @endif
+            <p class="card-text w-100">{{ str_limit(strip_tags($post->content), 150, '...') }}</p>
+          </section>
+          <section class="card-footer d-flex flex-wrap align-items-center justify-content-center">
+            <a href="{{ route('conversation', ['id' => $post->owner->id]) }}" class="card-link btn btn-link">{{ ucfirst(__('contact scout')) }}</a>
+            <a href="/post/{{ $post->id }}" class="card-link btn btn-link">{{ ucfirst(__('read more')) }}</a>
+            @if (Auth::user()->id === $post->user_id)
+            <a href="/post/{{ $post->id }}/edit" class="d-block w-100 mt-2 card-link btn btn-success">{{ ucfirst(__('edit')) }}</a>
+            @endif
+          </section>
+        </article>
+      </div>
+    --}}
 
-        <div class="col-md-4 mt-2 mb-2">
-          <article class="card h-100">
-            <section class="card-body d-flex flex-wrap">
-              <h4 class="card-title w-100">{{ title_case($post->title) }}</h4>
-              @if (Auth::user()->id === $post->user_id)
-                <h6 class="card-subtitle w-100 mb-2 text-muted">{{ ucfirst(__('written by')) }} {{ strtoupper(__('you')) }}</h6>
-              @else
-                <h6 class="card-subtitle w-100 mb-2 text-muted">{{ ucfirst(__('written by')) }} {{ $post->owner->name }}</h6>
-              @endif
-              <p class="card-text w-100">{{ str_limit(strip_tags($post->content), 150, '...') }}</p>
-            </section>
-            <section class="card-footer d-flex flex-wrap align-items-center justify-content-center">
-              <a href="{{ route('conversation', ['id' => $post->owner->id]) }}" class="card-link btn btn-link">{{ ucfirst(__('contact scout')) }}</a>
-              <a href="/post/{{ $post->id }}" class="card-link btn btn-link">{{ ucfirst(__('read more')) }}</a>
-              @if (Auth::user()->id === $post->user_id)
-              <a href="/post/{{ $post->id }}/edit" class="d-block w-100 mt-2 card-link btn btn-success">{{ ucfirst(__('edit')) }}</a>
-              @endif
-            </section>
-          </article>
+    <a href="{{ route('post', ['id' => $post->id]) }}" class="col-12 col-md-6">
+      <article class="post-card">
+        <div class="post-card__info">
+          @if (Auth::user()->id === $post->user_id)
+          <p class="post-card__author">{{ ucfirst(__('written by')) }} {{ strtoupper(__('you')) }}</p>
+          @else
+          <p class="post-card__author">{{ ucfirst(__('written by')) }} {{ $post->owner->name }}</p>
+          @endif
+          <h2 class="post-card__title">{{ str_limit(title_case($post->title), 40) }}</h2>
         </div>
+        <figure class="post-card__frame">
+          <img src="https://source.unsplash.com/random/{{ rand(600, 800) }}x{{ rand(600, 800) }}" alt="unsplash" class="post-card__frame__img">
+        </figure>
+      </article>
+    </a>
+  @endforeach
 
-      @endforeach
-
-    </div>
-  </main>
+</div>
 @endsection
