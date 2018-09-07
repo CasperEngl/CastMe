bcb211030b8a51630e1500245a5a2920021ca003
Casper Engelmann <me@casperengelmann.com>
2018-09-07 12:13:09 +0200

    Added blade directives for different roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Cashier\Cashier;
+use Blade;
+use Auth;
 
 class AppServiceProvider extends ServiceProvider {
   /**
@@ -14,6 +16,26 @@ class AppServiceProvider extends ServiceProvider {
   public function boot() {
     Cashier::useCurrency('DKK', 'kr');
     date_default_timezone_set('Europe/Amsterdam');
+
+    Blade::if('free', function () {
+      return !in_array(Auth::user()-role, ['Free']);
+    });
+
+    Blade::if('paid', function () {
+      return in_array(Auth::user()->role, ['Admin', 'Moderator', 'Scout', 'Paid']);
+    });
+
+    Blade::if('scout', function () {
+      return in_array(Auth::user()->role, ['Admin', 'Moderator', 'Scout']);
+    });
+
+    Blade::if('moderator', function() {
+      return in_array(Auth::user()->role, ['Admin', 'Moderator']);
+    });
+
+    Blade::if('admin', function () {
+      return in_array(Auth::user()->role, ['Admin']);
+    });
   }
 
   /**
