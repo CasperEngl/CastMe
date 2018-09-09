51be9c357b728d366b2a7ac323a0d357f5ecab61
Casper Engelmann <me@casperengelmann.com>
2018-09-10 00:38:47 +0200

    fuck it. updates to things. too tired to deal with this shit

--- resources/views/post/singular.blade.php
+++ resources/views/post/singular.blade.php
@@ -8,9 +8,9 @@ use App\Helpers\Format;
 @section('content')
 <article class="post-article">
   <div class="d-flex flex-column align-items-center">
-    <h6 class="post-article__date">{{ Carbon::parse($post->created_at)->toFormattedDateString() }}</h6>
+    <h6 class="post-article__date">{{ Carbon::parse($post->created_at)->format('M j, Y \a\t G:i a') }}</h6>
     @if (Carbon::parse($post->created_at)->timestamp !== Carbon::parse($post->updated_at)->timestamp)
-    <p class="text-muted"><em>{{ ucfirst(__('updated at')) }} {{ Carbon::parse($post->updated_at)->toDateTimeString() }}</em></p>
+    <p class="text-muted"><em>{{ ucfirst(__('updated at')) }} {{ Carbon::parse($post->updated_at)->format('M j, Y \a\t G:i a') }}</em></p>
     @endif
     <h1 class="post-article__title">{{ title_case($post->title) }}</h1>
     <blockquote class="post-article__quote">
@@ -22,18 +22,30 @@ use App\Helpers\Format;
     @if ($post->roles)
     <div class="d-flex align-items-center my-3">
       @foreach (json_decode($post->roles) as $role)
-      <span class="badge badge-pill badge-primary py-2 px-3 mx-1">{{ strtoupper($role) }}</span> @endforeach
+      <span class="badge badge-pill badge-primary py-2 px-3 mx-1">{{ strtoupper(__($role)) }}</span>
+      @endforeach
     </div>
     @endif
     <figure class="post-article__frame">
       <img src="{{ Storage::disk('public')->url($post->banner) }}" alt="" class="post-article__frame__img">
     </figure>
 
-    <div class="container mt-3">      
+    @if ($owner)
+    <div class="my-2 align-self-start">
+      <a href="{{ route('post.edit', ['id' => $post->id]) }}" class="btn btn-success">{{ ucfirst(__('edit')) }}</a>
+      @if ($post->closed)
+        <a href="{{ route('post.enable', ['id' => $post->id]) }}" class="btn btn-info">{{ ucfirst(__('release')) }}</a>
+      @else
+        <a href="{{ route('post.disable', ['id' => $post->id]) }}" class="btn btn-danger">{{ ucfirst(__('disable')) }}</a>
+      @endif
+    </div>
+    @endif
+
+    <div class="post-article__content">      
       {!! $post->content !!}
 
       <div class="d-flex flex-column">
-      @if ($post->images)
+      @if (is_array($post->images))
         <h6 class="text-muted mt-4">{{ ucfirst(__('images')) }}</h6>
         @foreach (json_decode($post->images) as $image)
         <a href="{{ $image }}" target="_blank">{{ Format::stripDomain($image) }}</a>
@@ -68,7 +80,7 @@ use App\Helpers\Format;
     @endforeach
   @else
   {{-- If nocomments are found --}}
-  <h2 class="page-header mb-0">{{ title_case(__('No comments')) }}</h2>
+  <h2 class="page-header mb-0">{{ ucfirst(__('no comments')) }}</h2>
   @endif
 @else
   @foreach ($comments as $comment)
@@ -81,7 +93,7 @@ use App\Helpers\Format;
       </div>
       <div class="card-footer">
         <div class="row align-items-center">
-          <div class="col">{{ Carbon::parse($comment->updated_at)->format('M j \a\t G:i') }}</div>
+          <div class="col">{{ Carbon::parse($comment->updated_at)->format('M j, Y \a\t G:i a') }}</div>
           <div class="col-auto">
             <a href="{{ route('conversation', ['id' => $comment->user_id]) }}" class="btn btn-primary">{{ ucfirst(__('message')) }}</a>
           </div>
