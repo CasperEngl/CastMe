f028a0a397bc43c1d39f863690ebcfc8085bc11e
Casper Engelmann <me@casperengelmann.com>
2018-09-10 14:28:55 +0200

    Added some HTML semantics

--- resources/views/post/singular.blade.php
+++ resources/views/post/singular.blade.php
@@ -8,11 +8,13 @@ use App\Helpers\Format;
 @section('content')
 <article class="post-article">
   <div class="d-flex flex-column align-items-center">
-    <h6 class="post-article__date">{{ Carbon::parse($post->created_at)->format('M j, Y \a\t G:i a') }}</h6>
-    @if (Carbon::parse($post->created_at)->timestamp !== Carbon::parse($post->updated_at)->timestamp)
-    <p class="text-muted"><em>{{ ucfirst(__('updated at')) }} {{ Carbon::parse($post->updated_at)->format('M j, Y \a\t G:i a') }}</em></p>
-    @endif
-    <h1 class="post-article__title">{{ title_case($post->title) }}</h1>
+    <hgroup class="d-flex flex-column align-items-center">
+      <h5 class="post-article__date">{{ Carbon::parse($post->created_at)->format('M j, Y \a\t G:i a') }}</h5>
+      @if (Carbon::parse($post->created_at)->timestamp !== Carbon::parse($post->updated_at)->timestamp)
+      <h6 class="text-muted"><em>{{ ucfirst(__('updated')) }} {{ Carbon::parse($post->updated_at)->format('M j, Y \a\t G:i a') }}</em></h6>
+      @endif
+      <h1 class="post-article__title">{{ title_case($post->title) }}</h1>
+    </hgroup>
     <blockquote class="post-article__quote">
       <figure class="post-article__quote__avatar">
         <img src="{{ Storage::disk('public')->url($post->owner->avatar) }}" alt="">
@@ -20,39 +22,39 @@ use App\Helpers\Format;
       <p class="post-article__quote__name">{{ $post->owner->name }} {{ $post->owner->last_name }}</p>
     </blockquote>
     @if ($post->roles)
-    <div class="d-flex align-items-center my-3">
+    <section class="d-flex align-items-center my-3">
       @foreach (json_decode($post->roles) as $role)
       <span class="badge badge-pill badge-primary py-2 px-3 mx-1">{{ strtoupper(__($role)) }}</span>
       @endforeach
-    </div>
+    </section>
     @endif
     <figure class="post-article__frame">
       <img src="{{ Storage::disk('public')->url($post->banner) }}" alt="" class="post-article__frame__img">
     </figure>
 
     @if ($owner || in_array(Auth::user()->role, ['Admin', 'Moderator']))
-    <div class="my-2 align-self-start">
+    <section class="my-2 align-self-start">
       <a href="{{ route('post.edit', ['id' => $post->id]) }}" class="btn btn-success">{{ ucfirst(__('edit')) }}</a>
       @if ($post->closed)
         <a href="{{ route('post.enable', ['id' => $post->id]) }}" class="btn btn-info">{{ ucfirst(__('release')) }}</a>
       @else
         <a href="{{ route('post.disable', ['id' => $post->id]) }}" class="btn btn-danger">{{ ucfirst(__('disable')) }}</a>
       @endif
-    </div>
+    </section>
     @endif
 
-    <div class="post-article__content">      
+    <section class="post-article__content">      
       {!! $post->content !!}
 
       <div class="d-flex flex-column">
       @if (is_array($post->images))
-        <h6 class="text-muted mt-4">{{ ucfirst(__('images')) }}</h6>
+        <h5 class="text-muted mt-4">{{ ucfirst(__('images')) }}</h5>
         @foreach (json_decode($post->images) as $image)
         <a href="{{ $image }}" target="_blank">{{ Format::stripDomain($image) }}</a>
         @endforeach
       @endif
       </div>
-    </div>
+    </section>
   </div>
 </article>
 
