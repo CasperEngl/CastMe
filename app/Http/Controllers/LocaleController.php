4adb51d84a537eee15c31e7a9d4edeca8a96cd5a
Casper Engelmann <me@casperengelmann.com>
2018-09-13 15:30:56 +0200

    Use resource_path instead of base_path

--- app/Http/Controllers/LocaleController.php
+++ app/Http/Controllers/LocaleController.php
@@ -95,7 +95,7 @@ class LocaleController extends Controller {
 
     public function get($locale = null, Request $request) {
         if ($locale !== null) {
-            $file_path = base_path('resources/lang/' . $locale . '.json');
+            $file_path = resource_path('lang/' . $locale . '.json');
 
             if (File::exists($file_path)) {
                 $json = json_decode(file_get_contents($file_path), true);
