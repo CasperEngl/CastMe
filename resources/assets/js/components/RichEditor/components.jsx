8e7030ada5a51a969f4854ee648f74eb9082e1be
Casper Engelmann <me@casperengelmann.com>
2018-09-16 01:37:34 +0200

    Added RichEditor (not working yet tho)

---
+++ resources/assets/js/components/RichEditor/components.jsx
@@ -0,0 +1,38 @@
+/*
+eslint
+
+react/destructuring-assignment: 0,
+no-nested-ternary: 0,
+*/
+
+import React from 'react';
+import styled from 'react-emotion';
+
+export const Button = styled('span')`
+  cursor: pointer;
+  color: ${props => (props.reversed
+    ? props.active ? 'white' : '#AAA'
+    : props.active ? 'black' : '#CCC')};
+`;
+
+export const Icon = styled(({ className, ...rest }) => <span className={`material-icons ${className}`} {...rest} />)`
+  font-size: 18px;
+  vertical-align: text-bottom;
+`;
+
+export const Menu = styled('div')`
+  & > * {
+    display: inline-block;
+  }
+  & > * + * {
+    margin-left: 15px;
+  }
+`;
+
+export const Toolbar = styled(Menu)`
+  position: relative;
+  padding: 1px 18px 17px;
+  margin: 0 -20px;
+  border-bottom: 2px solid #eee;
+  margin-bottom: 20px;
+`;
