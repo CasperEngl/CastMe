68acd2cceee7baa1dbdc96d99bbabb993fc5d52e
Casper Engelmann <me@casperengelmann.com>
2018-10-04 18:24:35 +0200

    Moved simple and advanced first

--- resources/assets/js/tm-editors.js
+++ resources/assets/js/tm-editors.js
@@ -3,35 +3,6 @@ import tinymce from 'tinymce';
 import './tinymce-plugins/charactercount';
 
 tinymce.init({
-  selector: 'textarea.tinymce',
-  theme: 'modern',
-  skin: 'lightgray',
-  branding: false,
-  paste_as_text: true,
-
-  width: '100%',
-  height: 500,
-
-  statubar: false,
-
-  plugins: [
-    'paste',
-    'lists',
-    'link',
-    'autolink',
-    'autosave',
-    'emoticons',
-    'fullscreen',
-    'image',
-    'insertdatetime',
-    'preview',
-    'wordcount',
-  ],
-
-  // toolbar: 'bold italic | alignleft aligncenter alignright alignjustify | link image | bullist numlist outdent indent',
-});
-
-tinymce.init({
   selector: 'textarea.tinymce.simple',
   paste_as_text: true,
   toolbar: false,
@@ -61,4 +32,33 @@ tinymce.init({
   ],
 
   toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons',
+});
+
+tinymce.init({
+  selector: 'textarea.tinymce',
+  theme: 'modern',
+  skin: 'lightgray',
+  branding: false,
+  paste_as_text: true,
+
+  width: '100%',
+  height: 500,
+
+  statubar: false,
+
+  plugins: [
+    'paste',
+    'lists',
+    'link',
+    'autolink',
+    'autosave',
+    'emoticons',
+    'fullscreen',
+    'image',
+    'insertdatetime',
+    'preview',
+    'wordcount',
+  ],
+
+  // toolbar: 'bold italic | alignleft aligncenter alignright alignjustify | link image | bullist numlist outdent indent',
 });
