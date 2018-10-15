357ff1a13964e0a61b2204ff150c0f8c145c5849
Casper Engelmann <me@casperengelmann.com>
2018-10-15 18:34:37 +0200

    Added Contact form

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.static')
 
 @section('content')
-
+<div class="container">
+  {{ Form::open(['route' => 'contact.post']) }}
+ 
+  <div class="form-group {{ $errors->has('name') ? 'has-error' : '' }}">
+    {{ Form::label(ucfirst(__('name'))) }}
+    {{ Form::text('name', old('name'), ['class' => 'form-control']) }}
+    <span class="text-danger">{{ $errors->first('name') }}</span>
+  </div>
+  
+  <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+    {{ Form::label(ucfirst(__('email'))) }}
+    {{ Form::text('email', old('email'), ['class' => 'form-control']) }}
+    <span class="text-danger">{{ $errors->first('email') }}</span>
+  </div>
+  
+  <div class="form-group {{ $errors->has('message') ? 'has-error' : '' }}">
+    {{ Form::label(ucfirst(__('message'))) }}
+    {{ Form::textarea('message', old('message'), ['class' => 'form-control']) }}
+    <span class="text-danger">{{ $errors->first('message') }}</span>
+  </div>
+  
+  <div class="form-group">
+    <button class="btn btn-castme">{{ ucfirst(__('contact')) }}</button>
+  </div>
+  
+  {{ Form::close() }}
+</div>
 @endsection
