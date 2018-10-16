b48d2bafe772c363641a8857b1556cc9b7a6b4c7
Jonatan <jonatantwn@gmail.com>
2018-10-16 18:50:39 +0200

    Guides text og css til accordions

--- resources/views/pages/guides.blade.php
+++ resources/views/pages/guides.blade.php
@@ -2,4 +2,128 @@
 
 @section('content')
 
+<div class="container content-wrapper">
+
+<h1>Guides og gode råd</h1>
+<p> Her kan du læse og se en række guides som hjælper dig til at formulere dig til det job du godt kunne tænke dig. Klik ganske enkelt på de områder, du gerne vil læse mere om.</p>
+
+    <div id="accordion">
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+            <h5 class="mb-0">
+                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Model
+                </button>
+            </h5>
+            </div>
+
+            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body"><p>
+            <b>Øvelse gør mester</b><br><br>
+
+            <b>Øv dig i at smile til spejlet.</b><br>
+            Eksperimenter med forskellige ansigtsudtryk og poseringer derhjemme.<br>
+            Få inspiration igennem blade, hjemmesider og reklamer. <br>
+            Tænk ud af boksen og skab dit eget look. <br><br>
+            
+            <b>Information omkring jobbet:</b><br>
+            Tjek firmaet/fotograf ud, om du vil arbejde videre med dem.<br>
+            Spørg ind til jobbet, om hvad det går ud på og hvad det skal bruges til.<br>
+            Vi ønsker du får en god oplevelse, så vær sikker på hvad du siger ja til.<br><br>
+
+            <b>Praktisk informationer:</b>
+            Spørg dem du har lavet en aftale med, om du skal man bære sit eget tøj og hvilken slags tøjstil ønsker fotograf/firma?<br><br>
+
+            <b>Hvad skal jeg have med på jobbet?</b><br>
+            Du skal ikke ændre på dig selv inden et shoot dvs. ingen klipning, farve hår eller lign. <br>
+            Tjek transport muligheder og rejsetiden dagen inden .<br><br>
+
+            <b>Kom forberedt: </b><br>
+            Husk at pakke en taske.<br>
+            Sørg for at få en god nats søvn.<br>
+            Inden du skal ud på job, sørg for at være 100 % på, det betyder hold dig fra saltet mad, junkfood og alkohol. <br>
+            Sørg for at spise mad som skaber god energi til jobbet f.eks noget proteinrigt. <br>
+            Held og lykke herfra og husk at få det bedste ud af det.  <br></p>
+
+            </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+            <h5 class="mb-0">
+                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Skuespiller
+                </button>
+            </h5>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div class="card-body"></p>
+            <b> Monolog:</b><br>
+            Man kan komme ud for at skulle fremlægge en monolog, her er nogle gode råd til det. <br><br>
+            
+            Det er en enetale og det modsatte af en dialog <br>
+            Identificere dig med karakteren og føl dig tilpas med det.  <br>
+            Vælg en tekst som er anderledes, spændende og underholdene som skal have handling.<br>
+            Der må godt være flere forskellige humør vendinger i teksten f.eks fra glad til sur eller bange. Det vigtigste er man ser energien. Så hold dig ikke tilbage.<br>
+            Du kan finde en monolog på nettet det kan f.eks være fra en film, teater og tv-serie.<br>
+            Teksten skal ca. være 1-5 minutter lang.<br>
+            Vær ikke bange for at overspille.<br>
+            Øv dig på stemmestyring og tone.<br>
+            Se en film og læg mærke til hvordan deres kropssprog er når præsentere deres replikker.<br><br>
+
+            <b>Forberedelse:</b>
+            Få styr på nervøsiteten.<br>
+            Se det som en udfordring og ikke som en forhindring.<br>
+            Øv dig hellere én gang for meget end en gang for lidt.<br>
+            Vær glad for situationen og smil, det viser overskud.<br>
+            Stol på instruktøren, han/hun vil jo kun det bedste for dig. <br>
+            Kom til tiden, både for dig og instruktøren.<br>
+            Forbered nogle spørgsmål til rollen, hvis du kommer på nogle, det viser engagement.<br>
+            Find evt. to måder at spille scenen på og spørg om du må prøve begge måder.<br><br>
+
+            Vigtigt at vide, man kan sagtens have gjort et godt stykke arbejde og alligvel få afslag, så bliv ikke ked af det men derimod spring op på hesten igen og prøv indtil den rigtige rolle rammer dig.<br>
+
+            </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingThree">
+            <h5 class="mb-0">
+                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Statist
+                </button>
+            </h5>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+            <div class="card-body">
+            <b>Hvad er en Statist?</b><br><br>
+
+            Opgaven kan variere alt efter om det er en spillefilm eller reklamefilm.<br>
+            Det kan være alt fra 1 time til mange timers indspilning afhængig af opgaven. <br>
+            Man kan komme ud for at få korte replikker, husk at forberede hvis det sker.<br>
+            I nogle tilfælde skal man have sit eget tøj på, men det bliver du informeret om inden optagelsen. <br>
+            Undlad iøjenfaldende tøj f.eks blomstret, ternet, og stribet som skaber for meget blikfang i tv´et medmindre du er blevet informeret anderledes.<br>
+
+            </div>
+            </div>
+
+            <div class="card">
+            <div class="card-header" id="headingThree">
+            <h5 class="mb-0">
+                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                Næste punkt (Test)
+                </button>
+            </h5>
+            </div>
+            <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+            <div class="card-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
 @endsection
