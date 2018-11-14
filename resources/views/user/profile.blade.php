ec6dce9278c3dba02978789c8d66bedb530ae78a
Casper Engelmann <me@casperengelmann.com>
2018-11-14 23:26:02 +0100

    Use columns in profile data

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -74,50 +74,74 @@
           <h4 class="profile__title">{{ ucfirst(__('Details')) }}</h4>
         </div>
         <div class="col-12">
-          @if ($user->details->ethnicity)
-          <p class="text-muted m-0">{{ ucfirst(__('ethnicity')) }}</p>
-          <p class="h5">{{ $user->details->ethnicity }}</p>
-          @endif
-          @if ($user->details->eye_color)
-          <p class="text-muted m-0">{{ ucfirst(__('eye color')) }}</p>
-          <p class="h5">{{ $user->details->eye_color }}</p>
-          @endif
-          @if ($user->details->hair_length)
-          <p class="text-muted m-0">{{ ucfirst(__('hair length')) }}</p>
-          <p class="h5">{{ $user->details->hair_length }}</p>
-          @endif
-          @if ($user->details->hair_color)
-          <p class="text-muted m-0">{{ ucfirst(__('hair color')) }}</p>
-          <p class="h5">{{ $user->details->hair_color }}</p>
-          @endif
-          @if ($user->details->age)
-          <p class="text-muted m-0">{{ ucfirst(__('age')) }}</p>
-          <p class="h5">{{ $user->details->age }}</p>
-          @endif
-          @if ($user->details->height)
-          <p class="text-muted m-0">{{ ucfirst(__('height')) }}</p>
-          <p class="h5">{{ $user->details->height }}</p>
-          @endif
-          @if ($user->details->weight)
-          <p class="text-muted m-0">{{ ucfirst(__('weight')) }}</p>
-          <p class="h5">{{ $user->details->weight }}</p>
-          @endif
-          @if ($user->details->experience)
-          <p class="text-muted m-0">{{ ucfirst(__('experience')) }}</p>
-          <p class="h5">{{ $user->details->experience }}</p>
-          @endif
-          @if ($user->details->pant_size)
-          <p class="text-muted m-0">{{ ucfirst(__('pants size')) }}</p>
-          <p class="h5">{{ $user->details->pant_size }}</p>
-          @endif
-          @if ($user->details->shoe_size)
-          <p class="text-muted m-0">{{ ucfirst(__('shoe size')) }}</p>
-          <p class="h5">{{ $user->details->shoe_size }}</p>
-          @endif
-          @if ($user->details->shirt_size)
-          <p class="text-muted m-0">{{ ucfirst(__('shirt size')) }}</p>
-          <p class="h5">{{ $user->details->shirt_size }}<p>
-          @endif
+          <div class="row">
+              @if ($user->details->ethnicity)
+              <div class="col-12">
+                <p class="text-muted m-0">{{ ucfirst(__('ethnicity')) }}</p>
+                <p class="h4">{{ $user->details->ethnicity }}</p>
+              </div>
+              @endif
+              @if ($user->details->eye_color)
+              <div class="col-12">
+                <p class="text-muted m-0">{{ ucfirst(__('eye color')) }}</p>
+                <p class="h4">{{ $user->details->eye_color }}</p>
+              </div>
+              @endif
+              @if ($user->details->hair_length)
+              <div class="col-12">
+                <p class="text-muted m-0">{{ ucfirst(__('hair length')) }}</p>
+                <p class="h4">{{ $user->details->hair_length }}</p>
+              </div>
+              @endif
+              @if ($user->details->hair_color)
+              <div class="col-12">
+                <p class="text-muted m-0">{{ ucfirst(__('hair color')) }}</p>
+                <p class="h4">{{ $user->details->hair_color }}</p>
+              </div>
+              @endif
+              @if ($user->details->age)
+              <div class="col-12">
+                <p class="text-muted m-0">{{ ucfirst(__('age')) }}</p>
+                <p class="h4">{{ $user->details->age }}</p>
+              </div>
+              @endif
+              @if ($user->details->height)
+              <div class="col-12">
+                <p class="text-muted m-0">{{ ucfirst(__('height')) }}</p>
+                <p class="h4">{{ $user->details->height }}</p>
+              </div>
+              @endif
+              @if ($user->details->weight)
+              <div class="col-12">
+                <p class="text-muted m-0">{{ ucfirst(__('weight')) }}</p>
+                <p class="h4">{{ $user->details->weight }}</p>
+              </div>
+              @endif
+              @if ($user->details->experience)
+              <div class="col-12">
+                <p class="text-muted m-0">{{ ucfirst(__('experience')) }}</p>
+                <p class="h4">{{ $user->details->experience }}</p>
+              </div>
+              @endif
+              @if ($user->details->pant_size)
+              <div class="col-12">
+                <p class="text-muted m-0">{{ ucfirst(__('pants size')) }}</p>
+                <p class="h4">{{ $user->details->pant_size }}</p>
+              </div>
+              @endif
+              @if ($user->details->shoe_size)
+              <div class="col-12">
+                <p class="text-muted m-0">{{ ucfirst(__('shoe size')) }}</p>
+                <p class="h4">{{ $user->details->shoe_size }}</p>
+              </div>
+              @endif
+              @if ($user->details->shirt_size)
+              <div class="col-12">
+                <p class="text-muted m-0">{{ ucfirst(__('shirt size')) }}</p>
+                <p class="h4">{{ $user->details->shirt_size }}<p>
+              </div>
+              @endif
+          </div>
         </div>
       </section>
     </div>
