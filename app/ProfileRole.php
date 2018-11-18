cfc3959e6b2f5424c1bd9382522b2fbd236eb64b
Casper Engelmann <me@casperengelmann.com>
2018-11-18 01:13:34 +0100

    get possible roles

--- app/ProfileRole.php
+++ app/ProfileRole.php
@@ -3,9 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class ProfileRole extends Model {
     protected $fillable = ['user_id', 'role'];
+
+    public static function getPossibleRoles() {
+      $type = DB::select(DB::raw("SHOW COLUMNS FROM profile_roles WHERE Field = 'role'") )[0]->Type;
+  
+      preg_match('/^enum\((.*)\)$/', $type, $matches);
+  
+      $enum = array();
+  
+      foreach(explode(',', $matches[1]) as $value) {
+        $val = trim($value, "'");
+        $enum = array_add($enum, str_slug($val, '_'), str_slug($val, '_')); 
+      }
+  
+      return $enum;
+    }
 
     public function user() {
         return $this->hasOne('App\User');
