6d98bec1cffb776fa2c612d878724f8eacd4a0cd
Jonatan <jonatantwn@gmail.com>
2018-11-19 23:59:41 +0100

    bornholm, post region addition

--- database/migrations/2018_07_16_230530_create_posts_table.php
+++ database/migrations/2018_07_16_230530_create_posts_table.php
@@ -18,7 +18,7 @@ class CreatePostsTable extends Migration {
       $table->text('content')->nullable();
       $table->text('images');
       $table->text('location')->nullable();
-      $table->enum('region', ['capital area', 'zealand', 'funen', 'northern jutland', 'mid jutland', 'south denmark'])->nullable();
+      $table->enum('region', ['capital area', 'zealand', 'funen', 'northern jutland', 'mid jutland', 'south denmark', 'bornholm'])->nullable();
       $table->string('banner')->default('placeholder/banner.png');
       $table->boolean('closed')->default(0);
       $table->timestamps();
