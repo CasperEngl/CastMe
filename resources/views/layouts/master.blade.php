e89a8a4a9bffea6bf5da05699c3ea5ed59e69c96
Casper Engelmann <me@casperengelmann.com>
2018-11-26 18:51:08 +0100

    Added profiles to sidebar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -135,6 +135,7 @@
           <div class="list-group mb-4">
             <a href="{{ route('overview') }}" class="list-group-item {{ active_route('overview', true) }}">{{ title_case(__('overview')) }}</a>
             <a href="{{ route('posts') }}" class="list-group-item {{ active_routes(['posts', str_singular('posts')], true) }}">{{ title_case(__('posts')) }}</a>
+            <a href="{{ route('profiles') }}" class="list-group-item {{ active_routes(['profiles', str_singular('profiles')], true) }}">{{ title_case(__('profiles')) }}</a>
           </div>
           <div class="list-group mb-4">
             <a href="{{ route('user.settings') }}" class="list-group-item {{ active_route('user.settings', true) }}">{{ title_case(__('profile settings')) }}</a>
