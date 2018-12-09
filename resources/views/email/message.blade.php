5ed47556085bd6eb3bab862ce423c38ea670fdee
Casper <me@casperengelmann.com>
2018-12-09 21:55:17 +0100

    Simplified the message a bit more

--- resources/views/email/message.blade.php
+++ resources/views/email/message.blade.php
@@ -2,6 +2,3 @@
 
 <p>{{ $sender->name }} {{ __('sent a new message to you.') }}</p>
 <p>{{ sentence(__('if you wish to read the conversation, you can click')) }} <a href="{{ env('APP_URL') }}/conversation/{{ $conversation->id }}">{{ __('here') }}</a></p>
-
-<p>{{ sentence(__('in case the link above doesn\'t work, try copy and pasting the following link into your browser address bar.')) }}</p>
-<p>{{ env('APP_URL') }}/conversation/{{ $conversation->id }}</p>
