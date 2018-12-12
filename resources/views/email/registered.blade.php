d6f7698d1a64ff47d21a6d74994a44599dcb8afb
Casper Engelmann <me@casperengelmann.com>
2018-12-12 08:24:07 +0100

    Added disclaimer that the agent can't login to their user

--- resources/views/email/registered.blade.php
+++ resources/views/email/registered.blade.php
@@ -2,3 +2,4 @@
 
 <p>{{ $registrant->name }} {{ $registrant->last_name }} {{ __('registered you.') }}</p>
 <p>{{ sentence(__('to log in, please reset your password')) }} <a href="{{ env('APP_URL') }}/password/reset/">{{ __('here') }}</a></p>
+<p style="font-style: italic; color: #ccc;">{{ sentence(__('the agent that registered you can not login to your account.')) }}</p>
