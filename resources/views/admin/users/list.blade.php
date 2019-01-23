98123f53052cad552fd72732919205374ed0f5b7
Casper Engelmann <cme@web-koncept.dk>
2019-01-23 07:56:52 +0100

    added role

--- resources/views/admin/users/list.blade.php
+++ resources/views/admin/users/list.blade.php
@@ -14,6 +14,7 @@
             <th scope="col">Name</th>
             <th scope="col">Email</th>
             <th scope="col">Phone</th>
+            <th scope="col">Role</th>
             <th scope="col">Created By</th>
             </tr>
         </thead>
@@ -21,10 +22,11 @@
             @foreach ($users as $user)
             <tr>
                 <th scope="row">{{ $user->id }}</th>
-                <td><img src="{{ Storage::disk('public')->url($user->avatar) }}" alt="{{ ucfirst(__('avatar')) }}"></td>
+                <td><img src="{{ Storage::disk('public')->url($user->avatar) }}" alt="{{ ucfirst(__('avatar')) }}" height="30"></td>
                 <td><a href="{{ route('profile', ['id' => $user->id]) }}">{{ $user->name }} {{ $user->last_name }}</a></td>
                 <td>{{ $user->email }}</td>
                 <td>{{ $user->phone }}</td>
+                <td>{{ $user->role }}</td>
                 @if ($user->createdBy)
                 <td><a href="{{ route('profile', ['id' => $user->createdBy->id]) }}">{{ $user->createdBy->name }} {{ $user->createdBy->last_name }}</a></td>
                 @else
