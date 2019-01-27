3da8eff0a589c3460007ec901169540c6c22f9e2
Jonatan <jonatantwn@gmail.com>
2019-01-27 20:46:39 +0100

    PHASES integration rettelse. Hidden Stripe, sections created.

--- resources/views/user/subscription.blade.php
+++ resources/views/user/subscription.blade.php
@@ -6,6 +6,44 @@
     <div class="card-body">
       <h2 class="page-header">{{ ucfirst(__('subscription')) }}</h2>
 
+      <!--
+ /PHASES INTEGRATION AREA START
+ -->
+
+      <button class="card-footer btn btn-castme" type="submit">
+      {{ ucfirst(__('subscribe start')) }}
+    </button>
+
+    <button class="card-footer btn btn-castme" type="submit">
+      {{ ucfirst(__('subscribe end')) }}
+    </button>
+
+    <section class="mt-4 mb-0">
+
+<h2 class="text-center">Abonnements detaljer</h2>
+   <p class="text-center">
+     Dit abonnement slutter d. XX.XX.XXXX
+   </p>
+</section>
+
+        </div>
+    </div>
+</div>
+
+
+<!--
+ /PHASES INTEGRATION AREA END
+ -->
+
+<!--
+ /STRIPE HIDE START
+ -->
+<div class="display-none">
+  <div class="card">
+    <div class="card-body">
+      <h2 class="page-header">{{ ucfirst(__('subscription')) }}</h2>
+
+
       <script src="https://js.stripe.com/v3/"></script>
       
       <div class="row">
@@ -68,8 +106,15 @@
       {{ ucfirst(__('subscribe')) }}
     </button>
   </div>
+  
 
   @csrf
   @method('POST')
 </form>
+
 @endsection
+
+<!--
+ /SLUT STRIPE HIDE
+ -->
+ </div> 
